aea04299afb5825b18f7e56b40f5c98f1c07cd1d
LuisMejiaSDTE <101164955+LuisMejiaSDTE@users.noreply.github.com>
2022-03-21 09:41:46 -0700

    Listo

---
+++ source/router/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+router.get('/',(req,res)=>{
+    res.send("Iniciamos Servidor");
+    router.get('/index',(req,res)=>{
+        res.render('index.html',{titulo:'Index'})
+    });
+    router.get('/acercade',(req,res)=>{
+        res.render('acercade.html',{titulo:'Acerca de'})
+    });
+    router.get('/contactos',(req,res)=>{
+        res.render('contactos.html',{titulo:'Contactos'})
+    });
+    router.get('*',(req,res)=>{
+    res.send("No existe la pagina");
+})
+})
+module.exports=router;
